e0e1b2fa6ae7912df866557c61795b81ce53e63d
chrajeswari <ch.raji@32003@gmail.com>
2023-11-20 20:35:24 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def raj(request):
+    return render(request,'raj.html')
+
+def sweety(request):
+    return render(request,'sweety.html')
+def raj2(request):
+    return render(request,'raj2.html')
